979d7d1741fe2919d8778f1da52f3edc54471ead
Ben Robison <brobison@adobe.com>
2018-02-19 10:08:50 -0700

    Added longform view for easier copy/paste

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var refresh = require('./routes/refresh_data');
+var longForm = require('./routes/long_form');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/refresh', refresh);
+app.use('/longform', longForm);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/long_form.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var cheerio = require('cheerio');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+  var output = [];
+
+  var contents = fs.readFileSync('./cache/masternodes.online.html', 'utf8'); 
+  var $ = cheerio.load(contents);    
+  
+  var coins = ['END', 'NIHL', 'IFX', 'ZOC', 'NYXCOIN', 'CTF', 'CROP', 'ARGO', 'XAR',
+               'RACE', 'QBIC', 'CBS', 'ESCO', 'SAROS', 'NMS', 'YI', 'CARAT', 'VTAR' 
+              ];
+  $(coins).each(function(index, coin){
+    
+    var $cells = $('tr:contains("'+coin+'") td');
+    var coinName = $cells.eq(2).text();
+    var coinPrice = $cells.eq(3).children('span').attr('title');
+    //coinPrice = parseFloat(coinPrice.replace(/[\$,%]/g, ''));
+    var coinROI = $cells.eq(7).children('strong').children('span').attr('title');
+    var coinVolume = $cells.eq(5).children('span').attr('title');
+    var nodeCount = $cells.eq(8).children('span').attr('title');
+    var nodeWorth = $cells.eq(10).children('span').attr('title');
+    var volumeRatio = parseFloat(coinVolume.replace(/[\$,%]/g, ''))/parseFloat(nodeWorth.replace(/[\$,%]/g, ''));
+    
+    var coinData = {name: coinName, 
+                    price: coinPrice,
+                    roi: coinROI,
+                    nodes: nodeCount,
+                    volume: coinVolume,
+                    worth: nodeWorth,
+                    ratio: volumeRatio };
+    
+    output.push(coinData);
+
+  });
+
+  res.render('longForm', { title: 'Coin Data', data: output});
+});
+
+module.exports = router;
