26753a1f0286bb88d16cb13dc087d4dc974918eb
Ben Robison <brobison@adobe.com>
2018-02-19 10:52:36 -0700

    Coin values are numbers, with fixed decmals

--- routes/index.js
+++ routes/index.js
@@ -16,18 +16,20 @@ router.get('/', function(req, res, next) {
     
     var $cells = $('tr:contains("'+coin+'") td');
     var coinName = $cells.eq(2).text();
-    var coinPrice = $cells.eq(3).children('span').attr('title');
-    var coinROI = $cells.eq(7).children('strong').children('span').attr('title');
-    var coinVolume = $cells.eq(5).children('span').attr('title');
-    var nodeCount = $cells.eq(8).children('span').attr('title');
-    var nodeWorth = $cells.eq(10).children('span').attr('title');
+    var coinPrice = parseFloat($cells.eq(3).children('span').attr('title').replace(/[\$,%]/g, ''));
+    var coinROI = parseFloat($cells.eq(7).children('strong').children('span').attr('title').replace(/[\$,%]/g, ''));
+    var coinVolume = parseFloat($cells.eq(5).children('span').attr('title').replace(/[\$,%]/g, ''));
+    var nodeCount = parseFloat($cells.eq(8).children('span').attr('title').replace(/[\$,%]/g, ''));
+    var nodeWorth = parseFloat($cells.eq(10).children('span').attr('title').replace(/[\$,%]/g, ''));
+    var volumeRatio = coinVolume / nodeWorth;
     
     var coinData = {name: coinName, 
-                    price: coinPrice,
-                    roi: coinROI,
+                    price: coinPrice.toFixed(6),
+                    roi: coinROI.toFixed(2),
                     nodes: nodeCount,
-                    volume: coinVolume,
-                    worth: nodeWorth };
+                    volume: coinVolume.toFixed(3),
+                    worth: nodeWorth.toFixed(4),
+                    ratio: volumeRatio.toFixed(4) };
     
     output.push(coinData);
 
